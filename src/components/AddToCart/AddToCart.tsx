fc51dad42854e971dc3e079d6b41ba5906903426
efe <eefee1987@gmail.com>
2024-11-16 20:21:02 +0100

    add styles form

--- src/components/AddToCart/AddToCart.tsx
+++ src/components/AddToCart/AddToCart.tsx
@@ -1,7 +1,24 @@
 import './AddToCart.css';
 
 function AddToCart() {
-  return <div>AddToCart</div>;
+  return (
+    <section>
+      <p>Agrega los productos al carro de compra</p>
+      <form>
+        <input
+          className='quantity'
+          type='text'
+          placeholder='Cantidad'
+        />
+        <input
+          className='product-id'
+          type='text'
+          placeholder='Id del producto'
+        />
+        <button>Agregar</button>
+      </form>
+    </section>
+  );
 }
 
 export default AddToCart;
