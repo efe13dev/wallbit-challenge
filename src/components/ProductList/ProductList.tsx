405ef104a722373b27423c2875134d4a704bfc3a
efe <eefee1987@gmail.com>
2024-11-17 08:40:19 +0100

    delete state in productlist component

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,20 +1,10 @@
-import { useState, useEffect } from 'react';
 import { Product } from '../../types';
-import { getCartProducts } from '../../utils/getCartProduct';
 
 interface ProductListProps {
   products: Product[];
 }
 
 function ProductList({ products }: ProductListProps) {
-  const [_cartProducts, setCartProducts] = useState<Product[]>([]);
-
-  // Cargar productos al montar el componente
-  useEffect(() => {
-    const savedProducts = getCartProducts();
-    setCartProducts(savedProducts);
-  }, []);
-
   return (
     <div>
       <h2>Lista de Productos</h2>
